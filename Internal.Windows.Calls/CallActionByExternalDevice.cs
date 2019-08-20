0512c7387be2fbd0d0c3064724ba5e379c58190b
DjArt <art.letter.mail@gmail.com>
2019-08-21 00:14:20 +0700

    ActionByExternalDevice

---
+++ Internal.Windows.Calls/CallActionByExternalDevice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Windows.Calls
+{
+    public enum CallActionByExternalDevice : uint
+    {
+        None = 0,
+        Dialed = 1,
+        Accepted = 2,
+    }
+}
