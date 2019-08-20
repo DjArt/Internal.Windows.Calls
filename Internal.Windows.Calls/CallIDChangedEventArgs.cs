9f074075d873ab357f3f643b7801ddcd8d611a6b
DjArt <art.letter.mail@gmail.com>
2019-08-21 00:15:35 +0700

    EventArgs types

---
+++ Internal.Windows.Calls/CallIDChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace Internal.Windows.Calls
+{
+    public sealed class CallIDChangedEventArgs
+    {
+        public uint OldID { get; }
+        public uint NewID { get; }
+
+        internal CallIDChangedEventArgs(uint old, uint @new)
+        {
+            OldID = old;
+            NewID = @new;
+        }
+    }
+}
---
+++ Internal.Windows.Calls/CallTimeChangedEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Internal.Windows.Calls
+{
+    public sealed class CallTimeChangedEventArgs
+    {
+
+        public DateTimeOffset? OldTime { get; }
+        public DateTimeOffset? NewTime { get; }
+
+        internal CallTimeChangedEventArgs(DateTimeOffset? old, DateTimeOffset? @new)
+        {
+            OldTime = old;
+            NewTime = @new;
+        }
+    }
+}
