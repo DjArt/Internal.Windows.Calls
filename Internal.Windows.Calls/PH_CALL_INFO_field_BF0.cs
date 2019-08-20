4f77dda3d9b69de3f1a4e0d21c9fc3c454be5e3b
DjArt <art.letter.mail@gmail.com>
2019-08-21 00:15:07 +0700

    More enums

---
+++ Internal.Windows.Calls/PH_CALL_INFO_field_BF0.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Windows.Calls
+{
+    [Flags]
+    public enum PH_CALL_INFO_field_BF0 : uint
+    {
+        BreakthroughCall = 0x4,
+        ShowKeypadOnCall = 0x20,
+        ShowAssistedDialUI = 0x80,
+        CallForwarded = 0x200,
+        VoicemailCall  = 0x4000,
+    }
+}
---
+++ Internal.Windows.Calls/PhoneOm/PH_CALL_INFO_field_BDC.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internal.Windows.Calls.PhoneOm
+{
+    internal enum PH_CALL_INFO_field_BDC : uint
+    {
+        CallerIDValid = 2
+    }
+}
